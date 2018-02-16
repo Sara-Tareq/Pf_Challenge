a622f68106a09b335de08d46847494db4e73eb97
Sara Tareq <sara.tareq.ramadan@gmail.com>
2018-02-16 10:19:28 +0400

    adding property finder models

---
+++ app/src/main/java/test/com/pfchallenge/entities/Property.java
@@ -0,0 +1,33 @@
+package test.com.pfchallenge.entities;
+
+public class Property {
+
+	private int id;
+	private String price;
+	private String currency;
+	private int bathrooms;
+	private int bedrooms;
+
+	public Property() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public int getBathrooms() {
+		return bathrooms;
+	}
+
+	public int getBedrooms() {
+		return bedrooms;
+	}
+}
