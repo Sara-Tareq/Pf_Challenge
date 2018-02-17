4d6fefa43cd23020178873a8dbe3f330bc27ca89
Sara Tareq <sara.tareq.ramadan@gmail.com>
2018-02-17 10:06:56 +0400

    adding basic unit tests

--- app/src/test/java/test/com/pfchallenge/ExampleUnitTest.java
+++ app/src/test/java/test/com/pfchallenge/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package test.com.pfchallenge;
 
 import org.junit.Test;
 
+import test.com.pfchallenge.entities.Property;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,6 +12,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+	Property property = new Property("12000","USD",3,2);
+	@Test
+	public void testPropertyPrice(){
+		assertEquals("12000 USD",property.getPropertyPrice());
+	}
+
+	@Test
+	public void testPropertyBathrooms(){
+		assertEquals("3 Bathrooms",property.getPropertyBathrooms());
+	}
+
+	@Test
+	public void testPropertyBedrooms(){
+		assertEquals("2 Bedrooms",property.getPropertyBedrooms());
+	}
 	@Test
 	public void addition_isCorrect() throws Exception {
 		assertEquals(4, 2 + 2);
